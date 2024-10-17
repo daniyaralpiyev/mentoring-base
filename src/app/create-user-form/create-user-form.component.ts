3f9dbc7f37991f2f48e3f16303bffafde2b50377
Daniyar <piranha0590@gmail.com>
2024-10-18 02:41:38 +0600

    Angular Material, Модальные окна последний код без ошибок

--- src/app/create-user-form/create-user-form.component.ts
+++ src/app/create-user-form/create-user-form.component.ts
@@ -1,19 +1,16 @@
 import { NgFor, NgIf } from '@angular/common';
-import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MyErrorStateMatcher } from '../utils/error-state-matcher';
-import { MatDialog } from '@angular/material/dialog';
-import { CreateUserInterface } from '../interfaces/user-interfaces';
-import { CreateUserFormDialogComponent } from '../users-list/create-user-form-dialog/create-user-form-dialog.component';
 
 @Component({
   selector: 'app-create-user-form',
   standalone: true,
-  imports: [ReactiveFormsModule, NgIf, NgFor, MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule, CreateUserFormDialogComponent],
+  imports: [ReactiveFormsModule, NgIf, NgFor, MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule],
   templateUrl: './create-user-form.component.html',
   styleUrl: './create-user-form.component.scss'
 })
@@ -49,33 +46,5 @@ export class CreateUserFormComponent {
     this.createUserForm.emit(this.form.value);
     // reset очишает форму после добавления
     this.form.reset();
-  }
-
-  @Input()
-  public user!: CreateUserInterface;
-
-  @Output()
-  // используем в users-list.component.html
-  editUser = new EventEmitter<CreateUserInterface>();
-
-  // этот код взяли из сайта angular material из вкладки Dialog для диалогового окна
-  // MatDialog импортировали выше из angular material и так же переменную dialog используем ниже в методе openDialog()
-  readonly dialog = inject(MatDialog);
-
-
-  openCreateDialog(): void {
-    // EditUserDialogComponent это имя нашего компонента который сами создали
-    // this.dialog.open() открывает компонент EditUserDialogComponent и внутрь передаем данные (вкратце this.dialog.open() открывает модалку)
-    const dialogRef = this.dialog.open(CreateUserFormDialogComponent, {
-      data: { user: this.user },
-    });
-
-    // здесь когда компонент EditUserDialogComponent будет закрываться через subscribe отлавливаем событие
-    // и если во время закрытия передались данные то через emit передаем в наружний компонент
-    dialogRef.afterClosed().subscribe(editResult => {
-      if (editResult) {
-        this.editUser.emit(editResult);
-      };
-    });
   }
 }
--- src/app/users-list/create-user-form-dialog/create-user-form-dialog.component.ts
+++ src/app/users-list/create-user-form-dialog/create-user-form-dialog.component.ts
@@ -1,54 +1,12 @@
-import { Component, EventEmitter, inject, Output } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { MyErrorStateMatcher } from '../../utils/error-state-matcher';
-import { NgFor, NgIf } from '@angular/common';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { CreateUserInterface } from '../../interfaces/user-interfaces';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-create-user-form-dialog',
   standalone: true,
-  imports: [ReactiveFormsModule, NgIf, NgFor, MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule],
+  imports: [],
   templateUrl: './create-user-form-dialog.component.html',
   styleUrl: './create-user-form-dialog.component.scss'
 })
 export class CreateUserFormDialogComponent {
 
-  public readonly data = inject<{ user: CreateUserInterface }>(MAT_DIALOG_DATA);
-
-  // @Output() это декоратор, который помечает свойство компонента как исходящее событие
-  // new EventEmitter() это класс, который используется для создания и отправки событий
-  // и здесь createUserForm передали в user-list.component.html
-  @Output()
-  public createUserFormDialog = new EventEmitter();
-
-  // подключаем к html переменную form
-  // класс FormGroup обеденяет все FormControl так как FormControl это элемент класса FormGroup
-  public form = new FormGroup({
-
-    // каждую переменную класс FormControl будем передавать в файле html в тег input по названиям
-    // [Validators.required] поле обязательно для заполнения
-    // [Validators.email] поле ожидает обязательное заполнение c @
-    // [Validators.minLength(5)] поле ожидает минимум 5 символов
-    name: new FormControl('', [Validators.required, Validators.minLength(2)]),
-    email: new FormControl('', [Validators.required, Validators.email, Validators.minLength(5)]),
-    website: new FormControl('', [Validators.required, Validators.minLength(5)]),
-    company: new FormGroup({
-      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    }),
-  });
-
-  matcher = new MyErrorStateMatcher();
-
-  public submitForm(): void {
-    // emit() используется для генерации или отправки события от дочернего компонента к родительскому.
-    // Он вызывается на объекте EventEmitter, который был создан с помощью декоратора @Output().
-    this.createUserFormDialog.emit(this.form.value);
-    // reset очишает форму после добавления
-    this.form.reset();
-  }
 }
