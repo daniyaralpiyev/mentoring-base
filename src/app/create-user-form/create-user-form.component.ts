2912f56e14e01dd72ea87955ec6d444e9a0b9ce7
Daniyar <piranha0590@gmail.com>
2024-10-18 03:18:50 +0600

    Ангуляр материал с Владом код был исправлен

--- src/app/create-user-form/create-user-form.component.ts
+++ src/app/create-user-form/create-user-form.component.ts
@@ -1,50 +1,41 @@
 import { NgFor, NgIf } from '@angular/common';
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MyErrorStateMatcher } from '../utils/error-state-matcher';
+import { MatDialog } from '@angular/material/dialog';
+import { CreateUserInterface } from '../interfaces/user-interfaces';
+import { CreateUserFormDialogComponent } from '../users-list/create-user-form-dialog/create-user-form-dialog.component';
 
 @Component({
   selector: 'app-create-user-form',
   standalone: true,
-  imports: [ReactiveFormsModule, NgIf, NgFor, MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule],
+  imports: [ReactiveFormsModule, NgIf, NgFor, MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule, CreateUserFormDialogComponent],
   templateUrl: './create-user-form.component.html',
   styleUrl: './create-user-form.component.scss'
 })
 export class CreateUserFormComponent {
 
-  // @Output() это декоратор, который помечает свойство компонента как исходящее событие
-  // new EventEmitter() это класс, который используется для создания и отправки событий
-  // и здесь createUserForm передали в user-list.component.html
-  @Output()
-  public createUserForm = new EventEmitter();
-
-  // подключаем к html переменную form
-  // класс FormGroup обеденяет все FormControl так как FormControl это элемент класса FormGroup
-  public form = new FormGroup({
-
-    // каждую переменную класс FormControl будем передавать в файле html в тег input по названиям
-    // [Validators.required] поле обязательно для заполнения
-    // [Validators.email] поле ожидает обязательное заполнение c @
-    // [Validators.minLength(5)] поле ожидает минимум 5 символов
-    name: new FormControl('', [Validators.required, Validators.minLength(2)]),
-    email: new FormControl('', [Validators.required, Validators.email, Validators.minLength(5)]),
-    website: new FormControl('', [Validators.required, Validators.minLength(5)]),
-    company: new FormGroup({
-      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    }),
-  });
 
   matcher = new MyErrorStateMatcher();
 
-  public submitForm(): void {
-    // emit() используется для генерации или отправки события от дочернего компонента к родительскому.
-    // Он вызывается на объекте EventEmitter, который был создан с помощью декоратора @Output().
-    this.createUserForm.emit(this.form.value);
-    // reset очишает форму после добавления
-    this.form.reset();
+  @Output()
+  createUser = new EventEmitter<CreateUserInterface>();
+
+  readonly dialog = inject(MatDialog);
+
+  openCreateDialog(): void {
+    // открываем модалку. Ничего внутрь не передаем, а зачем нам передавать data { user }? мы же его только создаем. То, что ты передашь будет = undefined.
+    const dialogRef = this.dialog.open(CreateUserFormDialogComponent);
+
+    dialogRef.afterClosed().subscribe((result: CreateUserInterface) => {
+      if (result) { // если результат true (то-есть данные пришли), тогда эмитим(отправляем) эти данные в users-list
+        this.createUser.emit(result);
+        console.log('если result true, передаем эти данные:', result)
+      };
+    });
   }
 }
--- src/app/users-list/create-user-form-dialog/create-user-form-dialog.component.ts
+++ src/app/users-list/create-user-form-dialog/create-user-form-dialog.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, inject, Output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MyErrorStateMatcher } from '../../utils/error-state-matcher';
+import { NgFor, NgIf } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import { CreateUserInterface } from '../../interfaces/user-interfaces';
 
 @Component({
   selector: 'app-create-user-form-dialog',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgIf, NgFor, MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule, MatDialogModule],
   templateUrl: './create-user-form-dialog.component.html',
   styleUrl: './create-user-form-dialog.component.scss'
 })
 export class CreateUserFormDialogComponent {
+
+  public readonly data = inject<{ user: CreateUserInterface }>(MAT_DIALOG_DATA); // бесполезная фигня, мы никакие данные не получаем. Мы же не изменяем уже существующего пользователя 
+  // и не удаляем его по айди, как мы можем что-то передать, если на момент открытия модалки - его еще не существует. Надеюсь мысль уловил.
+
+
+  @Output()
+  public createUserFormDialog = new EventEmitter();
+
+  public form = new FormGroup({
+
+    name: new FormControl('', [Validators.required, Validators.minLength(1)]), // убрал пару валидаторов, что бы не вводить постоянно по 10 символов.
+    email: new FormControl('', [Validators.required, Validators.minLength(1)]),
+    website: new FormControl('', [Validators.required, Validators.minLength(1)]),
+    company: new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(1)]),
+    }),
+  });
+
+  matcher = new MyErrorStateMatcher();
 
 }
