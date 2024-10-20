e2742344452b69257ae237736a84df5ae151f493
Daniyar <piranha0590@gmail.com>
2024-10-20 06:14:12 +0600

    Ангуляр материал привязал к кнопке удаление todo

---
+++ src/app/todos-list/create-todo-form-dialog/create-todo-form-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-todo-form-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-todo-form-dialog.component.html',
+  styleUrl: './create-todo-form-dialog.component.scss'
+})
+export class CreateTodoFormDialogComponent {
+
+}
